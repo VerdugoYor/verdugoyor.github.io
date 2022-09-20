7c29896ca55bca120e5829d8ae3661db3623f536
yordanNV <yordanuve@gmail.com>
2022-09-20 11:36:34 -0300

    Actualización general

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// función de popovers
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
